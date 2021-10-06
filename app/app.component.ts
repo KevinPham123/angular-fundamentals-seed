e042b8ac5248e4259354e653672d1e6b549dbd46
Pham <KPham@viningsparks.com>
2021-10-06 13:22:56 -0500

    ngClass

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,23 +11,32 @@ interface Passenger {
   styleUrls: ['app.component.scss'],
   template: `
     <div class ="app">
-      <h3> Airline Passengers</h3>
-      <ul>
-        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
-        <li>
-        {{ i }}: {{ passenger.fullname }}
-        </li>
-        </template>
-        
-      </ul>
+ 
 
       <h3> Airline Passengers</h3>
       <ul>
         <li *ngFor = "let passenger of passengers; let i = index;">
+        <span 
+        class = "status"
+        [class.checked-in]="passenger.checkedIn"></span>
         {{ i }}: {{ passenger.fullname }}
 
         </li>
       </ul>
+
+      <h3> Airline Passengers</h3>
+      <ul>
+      <li *ngFor = "let passenger of passengers; let i = index;">
+      <span 
+      class = "status"
+      [ngClass]="{
+        'checked-in': passenger.checkedIn,
+        'checked-out': !passenger.checkedIn
+      }"></span>
+      {{ i }}: {{ passenger.fullname }}
+
+      </li>
+    </ul>
     </div>
   `
 })
