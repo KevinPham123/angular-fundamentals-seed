d54896efe1542d7c5b21d383eac6f30592ff69b9
Pham <KPham@viningsparks.com>
2021-10-08 16:51:37 -0500

    Http put, delete, and headers

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,6 +1,6 @@
 import { Passenger } from "./models/passenger.interface";
 import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
+import { Http, Response, Headers,RequestOptions } from "@angular/http";
 
 import { Observable } from "rxjs";
 import 'rxjs/add/operator/map';
@@ -18,6 +18,25 @@ export class PassengerDashboardService {
         .get(PASSENGER_API)
         .map((response: Response) => response.json())
     }
+
+    updatePassenger(passenger: Passenger): Observable<Passenger> {
+      let headers = new Headers({
+        'Content-Type': 'application/json'
+
+      });
+      let options = new RequestOptions({
+        headers: headers
+      });
+      return this.http
+      .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
+      .map((response: Response) => response.json());
+  }
+
+  removePassenger(passenger: Passenger): Observable<Passenger> {
+    return this.http
+    .delete(`${PASSENGER_API}/${passenger.id}`)
+    .map((response: Response) => response.json());
+}
     
 
 }
