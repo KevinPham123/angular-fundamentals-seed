ad4f6722019f3fc0bd77a9dc01f8bb13fff7325c
Pham <KPham@viningsparks.com>
2021-10-12 11:29:40 -0500

    ngForm/ ngModel

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -5,12 +5,28 @@ import { Passenger } from "../../models/passenger.interface";
     selector: 'passenger-form',
     styleUrls: ['passenger-form.component.scss'],
     template: `
-    <form>
-        Form!
-        <div>
+    <form #form="ngForm" novalidate>
         {{ detail | json}}
+        <div>
+            Passenger name:
+            <input
+            type="text"
+            name="fullname"
+            [ngModel]="detail?.fullname">
+
         </div>
-        
+
+
+        <div>
+            Passenger ID:
+            <input
+            type="text"
+            name="id"
+            [ngModel]="detail?.id">
+
+        </div>
+
+        {{ form.value | json}}
     </form>
     `
 
