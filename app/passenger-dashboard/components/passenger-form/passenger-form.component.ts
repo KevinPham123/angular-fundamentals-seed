79c620f4b7b3ba7db4a19c2ad5ba59c6393a5326
Pham <KPham@viningsparks.com>
2021-10-13 10:01:29 -0500

    ngOutput and submit stateless

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Passenger } from "../../models/passenger.interface";
 import { Baggage } from "../../models/baggage.interface";
 
@@ -6,7 +6,7 @@ import { Baggage } from "../../models/baggage.interface";
     selector: 'passenger-form',
     styleUrls: ['passenger-form.component.scss'],
     template: `
-    <form #form="ngForm" novalidate>
+    <form (ngSubmit)="handleSubmit(form.value, form.valid)" #form="ngForm" novalidate>
   
         <div>
             Passenger name:
@@ -91,6 +91,10 @@ export class PassengerFormComponent {
     @Input()
     detail: Passenger;
 
+    @Output()
+    update: EventEmitter<Passenger> = new EventEmitter<Passenger>();
+
+
     baggage: Baggage[] = [{
         key: 'none',
         value: 'No baggage'
@@ -115,6 +119,12 @@ export class PassengerFormComponent {
     toggleCheckIn(checkedIn: boolean){
         if(checkedIn) {
             this.detail.checkInDate =Date.now();
+        }
+    }
+
+    handleSubmit(passenger: Passenger, isValid: boolean){
+        if(isValid) {
+            this.update.emit(passenger);
         }
     }
 }
